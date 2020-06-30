eaf1dcac334208b4ea5fed8f7781bd8a30a2fbc5
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-30 14:04:19 -0700

    data seeded

---
+++ __tests__/memes.test.js
@@ -0,0 +1,22 @@
+const { MongoMemoryServer } = require('mongodb-memory-server');
+const mongod = new MongoMemoryServer();
+const mongoose = require('mongoose');
+const connect = require('../lib/utils/connect');
+const Meme = require('../lib/models/Meme.js');
+
+describe('extramember routes', () => {
+  beforeAll(async() => {
+    const uri = await mongod.getUri();
+    return connect(uri);
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(async() => {
+    await mongoose.connection.close();
+    return mongod.stop();
+  });
+
+});
