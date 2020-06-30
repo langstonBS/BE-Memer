5a96c07566d599d97cbc94a69a6123349c59e1f8
langstonBS <langston.beckwithstanley@gmail.com>
2020-06-30 14:56:03 -0700

    GET all Passees

--- __tests__/memes.test.js
+++ __tests__/memes.test.js
@@ -1,11 +1,10 @@
-require('../db/data-helpers');
+const { prepare } = require('../db/data-helpers');
 const app = require('../lib/app');
 const request = require('supertest');
+const Meme = require('../lib/models/Meme');
 
 describe('Meme routs', () => {
-  it('creates a meme via POST', async() => {
-      
-    
+  it('creates a meme via POST', async() => {  
     return request(app)
       .post('/api/v1/memes')
       .send({
@@ -21,6 +20,15 @@ describe('Meme routs', () => {
           bottom: 'this is the the bottom',
           __v: 0
         });
+      });
+  });
+  it('gets all Memes via GET', async() => {
+    const memes = prepare(await Meme.find());
+
+    return request(app)
+      .get('/api/v1/memes')
+      .then(res => {
+        expect(res.body).toEqual(memes);
       });
   });
 });
--- db/data-helpers.js
+++ db/data-helpers.js
@@ -23,3 +23,16 @@ afterAll(async() => {
   await mongoose.connection.close();
   return mongod.stop();
 });
+
+const prepareOne = model => JSON.parse(JSON.stringify(model));
+const prepareMany = models => models.map(prepareOne);
+
+const prepare = model => {
+  if(Array.isArray(model)) return prepareMany(model);
+  return prepareOne(model);
+};
+
+
+module.exports = {
+  prepare,
+};
